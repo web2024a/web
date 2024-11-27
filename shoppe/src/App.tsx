8be2608daf8eba0feb79a341a182ba2aab38cabd
9five7 <trucpro1282003@gmail.com>
2024-11-27 11:14:25 +0700

    code

---
+++ shoppe/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import useRouteElements from './useRouteElements'
+
+function App() {
+  const routeElements = useRouteElements()
+
+  return (
+    <div>
+      {routeElements}
+    </div>
+  )
+}
+
+export default App
---
+++ shoppe/src/layouts/RegisterLayout/RegisterLayout.tsx
@@ -0,0 +1,14 @@
+import RegisterHeader from '../../components/RegisterHeader'
+
+interface Props {
+  children?: React.ReactNode
+}
+
+export default function RegisterLayout({ children }: Props) {
+  return (
+    <div>
+      <RegisterHeader />
+      {children}
+    </div>
+  )
+}
